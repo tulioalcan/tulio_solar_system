360660dd8fd891906bfebdb22f8a48eec54444b9
Túlio Mendes <tulioalcan@gmail.com>
2022-09-21 15:57:41 -0300

    requisito 05

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{ planetName }</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
