0bcac75a25b3c41a28eba464cd9611e3a3b02d67
Túlio Mendes <tulioalcan@gmail.com>
2022-09-21 15:41:42 -0300

    requisito 04

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -5,7 +5,7 @@ class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <Title>Planetas</Title>
+        <Title headline="Planetas" />
       </div>
     );
   }
