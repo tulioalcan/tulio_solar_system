fb70d6d677c6a644f9b8ae95632334aa5ae4a26d
tulioalcan <tulioalcan@gmail.com>
2023-01-02 14:36:38 -0300

    editando css

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -6,11 +6,12 @@ import Title from './Title';
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
+      <div data-testid="missions" className="missions">
         <Title headline="Missões" />
         {missions.map((mission) => (
           <MissionCard
             key={ mission.name }
+            className={ mission.name }
             name={ mission.name }
             year={ mission.year }
             country={ mission.country }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -6,15 +6,17 @@ import Title from './Title';
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">
+      <div data-testid="solar-system" className="planets-container">
         <Title headline="Planetas" />
-        {planets.map((planet) => (
-          <PlanetCard
-            key={ planet.name }
-            planetImage={ planet.image }
-            planetName={ planet.name }
-          />
-        ))}
+        <div className="planets-container2">
+          {planets.map((planet) => (
+            <PlanetCard
+              key={ planet.name }
+              planetImage={ planet.image }
+              planetName={ planet.name }
+            />
+          ))}
+        </div>
       </div>
     );
   }
